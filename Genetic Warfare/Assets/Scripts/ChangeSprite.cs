20641718d29ea024fd1ad22309fbd19d0c89a37c
thermitejam <99jamesdixon@gmail.com>
2019-09-13 16:35:32 +0100

    Inventory Animation
    I added in a button and animation for the new Inventory

---
+++ Genetic Warfare/Assets/Scripts/ChangeSprite.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeSprite : MonoBehaviour
+{
+    Image spriterenderer;
+    Sprite Sprite1;
+    [SerializeField] Sprite Sprite2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriterenderer = gameObject.GetComponent<Image>();
+        Sprite1 = spriterenderer.sprite;
+    }
+
+    // Update is called once per frame
+    public void change()
+    {
+        if (spriterenderer.sprite == Sprite1)
+        {
+            spriterenderer.sprite = Sprite2;
+        }
+        else
+        {
+            spriterenderer.sprite = Sprite1;
+        }
+        
+    }
+}
